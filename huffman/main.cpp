8915846c08d6990fb8e96e43de0eb43f00c5fa5f
Michael Middlezong <119022671+mmiddlezong@users.noreply.github.com>
2023-07-22 16:46:29 -0400

    Update main.cpp

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <unordered_map>
 #include <sstream>
+#include <vector>
+#include <deque>
 using namespace std;
 
 struct Node
@@ -33,7 +36,7 @@ void swapNodes(Node **a, Node **b)
     *b = t;
 }
 
-void *enqueue(MinHeap *heap, Node *node)
+void enqueue(MinHeap *heap, Node *node)
 {
     if (heap->size >= heap->capacity)
     {
@@ -98,22 +101,29 @@ Node *createNode(char value, unsigned freq)
     node->left = node->right = NULL;
     return node;
 }
-void dfs(Node *cur, string path, unordered_map<char, string> &code)
+void generateCode(Node *cur, string path, unordered_map<char, string> &code)
 {
     if (cur->left)
     {
-        dfs(cur->left, path + "0", code);
+        generateCode(cur->left, path + '0', code);
     }
     if (cur->right)
     {
-        dfs(cur->right, path + "1", code);
+        generateCode(cur->right, path + '1', code);
     }
     if (!cur->left && !cur->right)
     {
         code[cur->value] = path;
     }
 }
-unordered_map<char, string> getHuffmanCode(unordered_map<char, unsigned> freqMap)
+
+unordered_map<char, string> getHuffmanCode(Node *huffmanTree)
+{
+    unordered_map<char, string> code;
+    generateCode(huffmanTree, "", code);
+    return code;
+}
+Node *generateHuffmanTree(const unordered_map<char, unsigned> &freqMap)
 {
     MinHeap *heap = createMinHeap(freqMap.size());
     for (auto i : freqMap)
@@ -132,11 +142,7 @@ unordered_map<char, string> getHuffmanCode(unordered_map<char, unsigned> freqMap
     }
 
     Node *huffmanTree = heap->arr[1];
-    delete heap;
-    unordered_map<char, string> code;
-    dfs(huffmanTree, "", code);
-
-    return code;
+    return huffmanTree;
 }
 
 unordered_map<char, unsigned> createFreqMap(const string &str)
@@ -151,12 +157,12 @@ unordered_map<char, unsigned> createFreqMap(const string &str)
 
 string encode(const string &str, const unordered_map<char, string> &code)
 {
-    stringstream res;
+    string res;
     for (const char &c : str)
     {
-        res << code.at(c);
+        res += code.at(c);
     }
-    return res.str();
+    return res;
 }
 
 unordered_map<string, char> createReverseLookup(const unordered_map<char, string> &code)
@@ -191,19 +197,233 @@ string decode(const string &str, const unordered_map<char, string> &code)
     return result.str();
 }
 
+void traverseTree(Node *cur, string &out)
+{
+    if (!cur->left && !cur->right)
+    {
+        out += '1';
+        char c = cur->value;
+        for (int i = 7; i >= 0; --i)
+        {
+            bool bit = (c >> i) & 1;
+            out += bit ? '1' : '0';
+        }
+    }
+    else
+    {
+        out += '0';
+        traverseTree(cur->left, out);
+        traverseTree(cur->right, out);
+    }
+}
+
+string serializeTree(Node *tree)
+{
+    string out;
+    traverseTree(tree, out);
+    return out;
+}
+
+Node *deserialize(const string &serializedTree, int &i)
+{
+    while (i < serializedTree.length())
+    {
+        // Read bit
+        bool b = serializedTree.at(i) == '1';
+
+        if (b)
+        {
+            char byte = 0;
+            for (int pos = 7; pos >= 0; pos--)
+            {
+                ++i;
+                b = serializedTree.at(i) == '1';
+                if (b)
+                {
+                    byte |= (1 << pos);
+                }
+            }
+            Node *newNode = createNode(byte, 0);
+            return newNode;
+        }
+        else
+        {
+            Node *newNode = createNode('$', 0);
+            newNode->left = deserialize(serializedTree, ++i);
+            newNode->right = deserialize(serializedTree, ++i);
+            return newNode;
+        }
+    }
+    return NULL;
+}
+Node *deserializeTree(const string &serializedTree)
+{
+    int i = 0;
+    return deserialize(serializedTree, i);
+}
+
+void compressFile(const string &inputPath, const string &outputPath)
+{
+    ifstream file(inputPath);
+    string inputString;
+    if (file.is_open())
+    {
+        stringstream buffer;
+        buffer << file.rdbuf();
+        inputString = buffer.str();
+        file.close();
+    }
+    else
+    {
+        cout << "Failed to open the file.\n";
+        return;
+    }
+
+    unordered_map<char, unsigned> freqMap = createFreqMap(inputString);
+    Node *tree = generateHuffmanTree(freqMap);
+    unordered_map<char, string> code = getHuffmanCode(tree);
+
+    string encoded = encode(inputString, code);
+    string buffer = serializeTree(tree);
+    unsigned int bufferSize = buffer.length(); // Get number of bits needed to store the tree
+    unsigned int encodedSize = encoded.length();
+
+    // Write the compressed file
+    ofstream out(outputPath, ios::binary);
+
+    out.write(reinterpret_cast<char *>(&bufferSize), sizeof(bufferSize));
+    out.write(reinterpret_cast<char *>(&encodedSize), sizeof(encodedSize));
+
+    char byte = 0;
+    int pos = 0;
+    for (const char &c : buffer)
+    {
+        bool b = c == '1';
+        if (b)
+        {
+            byte |= (1 << pos);
+        }
+        pos++;
+        if (pos == 8)
+        {
+            out.write(&byte, sizeof(byte));
+            byte = 0;
+            pos = 0;
+        }
+    }
+    if (pos > 0)
+    {
+        out.write(&byte, sizeof(byte));
+    }
+
+    byte = 0;
+    pos = 0;
+    for (const char &c : encoded)
+    {
+        bool b = c == '1';
+        if (b)
+        {
+            byte |= (1 << pos);
+        }
+        pos++;
+        if (pos == 8)
+        {
+            out.write(&byte, sizeof(byte));
+            byte = 0;
+            pos = 0;
+        }
+    }
+    if (pos > 0)
+    {
+        out.write(&byte, sizeof(byte));
+    }
+    out.close();
+    cout << "Compression successful.\n";
+    cout << "Original size: " << inputString.length() << " bytes\n";
+    cout << "Buffer size: " << (bufferSize + 7) / 8 << " bytes\n";
+    cout << "Encoded size: " << (encoded.size() + 7) / 8 << " bytes\n";
+}
+
+void decompressFile(const string &inputPath, const string &outputPath)
+{
+    ifstream compressed(inputPath, ios::binary);
+
+    unsigned int bufferSize;
+    unsigned int encodedSize;
+
+    compressed.read(reinterpret_cast<char *>(&bufferSize), sizeof(bufferSize));
+    compressed.read(reinterpret_cast<char *>(&encodedSize), sizeof(encodedSize));
+
+    char byte;
+    stringstream bufferStream, encodedStream;
+    while (bufferSize >= 8)
+    {
+        compressed.read(&byte, sizeof(byte));
+
+        // Add this to the string
+        for (int pos = 0; pos < 8; ++pos)
+        {
+            bool b = byte & (1 << pos);
+            bufferStream << (b ? '1' : '0');
+        }
+
+        bufferSize -= 8;
+    }
+    if (bufferSize > 0)
+    {
+        compressed.read(&byte, sizeof(byte));
+
+        // Add this to the string
+        for (int pos = 0; pos < bufferSize; ++pos)
+        {
+            bool b = byte & (1 << pos);
+            bufferStream << (b ? '1' : '0');
+        }
+    }
+    string serializedTree = bufferStream.str();
+
+    // Read the encoded string
+    while (encodedSize >= 8)
+    {
+        compressed.read(&byte, sizeof(byte));
+
+        // Add this to the string
+        for (int pos = 0; pos < 8; ++pos)
+        {
+            bool b = byte & (1 << pos);
+            encodedStream << (b ? '1' : '0');
+        }
+
+        encodedSize -= 8;
+    }
+    if (encodedSize > 0)
+    {
+        compressed.read(&byte, sizeof(byte));
+
+        // Add this to the string
+        for (int pos = 0; pos < encodedSize; ++pos)
+        {
+            bool b = byte & (1 << pos);
+            encodedStream << (b ? '1' : '0');
+        }
+    }
+    string encodedData = encodedStream.str();
+    compressed.close();
+
+    Node *deserializedTree = deserializeTree(serializedTree);
+    unordered_map<char, string> deserializedCode = getHuffmanCode(deserializedTree);
+    string decodedString = decode(encodedData, deserializedCode);
+    ofstream decodedFile(outputPath);
+    decodedFile << decodedString;
+    decodedFile.close();
+
+    cout << "Decompression successful.\n";
+}
+
 int main()
 {
-    string str = "349s5n8vbns43598vs49n8uovn49u8sovn9s8uo4vs89uno8u9onsvenu9ov8raw8uonwrecanuiohaewrwycauicaeyugbdcsafgvdsghsdghvcffghvcfghvdgfdsfcgvdscfghvaecfghvaecfwhgbfcadhbjdchjbdfchbfhgbdsfchjbdhjgbcdhj";
-    unordered_map<char, unsigned> freqMap = createFreqMap(str);
-    unordered_map<char, string> code = getHuffmanCode(freqMap);
-    for (auto i : code)
-    {
-        cout << i.first << " " << i.second << "\n";
-    }
-    string encoded = encode(str, code);
-    cout << "Encoded string: " << encoded << "\n";
-    cout << "Encoded size: " << encoded.length() / 8 << " bytes\n";
-    cout << "Decoded string: " << decode(encoded, code) << "\n";
-    cout << "Decoded size: " << str.length() << " bytes\n";
+    compressFile("input.txt", "compressed.bin");
+    decompressFile("compressed.bin", "decoded.txt");
+
     return 0;
 }
